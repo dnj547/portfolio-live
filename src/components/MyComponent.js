ff77592876682e48744d1c5a819eabfc22971df3
dnj547 <dnj547@live.com>
2019-07-31 15:44:58 -0400

    Remove pretty much everything

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div className="my-component">
+        MyComponent
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
