62f0defc631bdd39fbaa5b71ad043d8e89e1845c
dnj547 <dnj547@live.com>
2019-08-01 14:15:31 -0400

    Add picture and some text

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,7 +4,11 @@ class MyComponent extends Component {
   render() {
     return (
       <div className="my-component">
-        MyComponent
+        <img src="/images/me_circle.png" alt="me"/>
+        <div className="text-content">
+          <h1>Danielle Jasper</h1>
+          <p>Hi, I'm Danielle.</p>
+        </div>
       </div>
     );
   }
