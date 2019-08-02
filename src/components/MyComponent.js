9c1d7ddd1d2a1ee46351bb3a11a2bd026a4352f3
dnj547 <dnj547@live.com>
2019-08-02 13:23:20 -0400

    Fix education hover

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -24,26 +24,26 @@ class MyComponent extends Component {
             <h1>Danielle Jasper</h1>
             <p>Hi, I'm Danielle.</p>
           </Typist>
-          {this.state.educationHover ? (
-            <div className="education"
-              onMouseEnter={this.toggleEducationHover}
-              onMouseLeave={this.toggleEducationHover}>
-              <h1>Education</h1>
-            </div>
-            ) : (
-            <div className="education"
-              onMouseEnter={this.toggleEducationHover}
-              onMouseLeave={this.toggleEducationHover}>
-              <div className="fs-container">
-                <img className="fs-img" src="/images/fs-cutout.png" alt="Flatiron School"/>
-                <p>Software Engineering</p>
+          <div
+            onMouseEnter={this.toggleEducationHover}
+            onMouseLeave={this.toggleEducationHover}>
+            {!this.state.educationHover ? (
+              <div className="education">
+                <h1>Education</h1>
               </div>
-              <div className="lsu-container">
-                <img className="lsu-img" src="/images/lsu.png" alt="Louisiana State University"/>
-                <p>Accounting</p>
+              ) : (
+              <div className="education">
+                <div className="fs-container">
+                  <img className="fs-img" src="/images/fs-cutout.png" alt="Flatiron School"/>
+                  <p>Software Engineering</p>
+                </div>
+                <div className="lsu-container">
+                  <img className="lsu-img" src="/images/lsu.png" alt="Louisiana State University"/>
+                  <p>Accounting</p>
+                </div>
               </div>
-            </div>
-            )}
+              )}
+          </div>
         </div>
       </div>
     );
