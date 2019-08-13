04eb273b0960e6863324c2d6b312025f699b0798
Danielle <dnj547@live.com>
2019-08-13 18:26:33 -0400

    Update MyProjects.js

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -20,7 +20,7 @@ export default class MyProjects extends Component {
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            {this.props.data.name} | <a href={this.props.data.github}>GitHub</a> | <a href={this.props.data.planning}>Plans</a>
+            {this.props.data.name} | <a href={this.props.data.github} target="_blank">GitHub</a> | <a href={this.props.data.planning} target="_blank">Plans</a>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
@@ -30,7 +30,7 @@ export default class MyProjects extends Component {
           </p>
         </Modal.Body>
         <Modal.Footer>
-          <h6>Partner: <a href={this.props.data.partnerGithub}>{this.props.data.partner}</a> | Created: {this.props.data["date-created"]}</h6>
+          <h6>Partner: <a href={this.props.data.partnerGithub} target="_blank">{this.props.data.partner}</a> | Created: {this.props.data["date-created"]}</h6>
           <Button onClick={props.onHide}>Close</Button>
         </Modal.Footer>
       </Modal>
